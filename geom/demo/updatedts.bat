c2e9bb5814fadbafc2c13907d32e38925c49ffe6
frokhman <fajar.rokhman@verint.com>
2024-05-05 17:48:17 +0700

    geom

---
+++ geom/demo/updatedts.bat
@@ -0,0 +1,5 @@
+copy c:\tsmodule\be.d.ts .\src
+copy c:\tsmodule\geom.d.ts .\src
+copy c:\tsmodule\be.js .\web\js
+copy c:\tsmodule\geom.js .\web\js
+pause
---
+++ geom/lib/src/Poly.ts
@@ -0,0 +1,31 @@
+namespace ha.geom {
+	export class PolyObj {
+		private vs: IPoint2D[] = [];
+
+		constructor(vs: IPoint2D[]) {
+			vs.forEach((item) => {
+				this.vs.push(item);
+			});
+		}
+	}
+
+	/**
+	 * Point to Polygon
+	 */
+	class PlP {
+
+	}
+
+	export class Poly {
+		private static pp: PlP = new PlP();
+
+		static buat(p: IPoint2D[]): PolyObj {
+			this.pp;
+			return new PolyObj(p);
+		}
+	}
+
+
+	const Pl: Poly = Poly;
+	Pl;
+}
