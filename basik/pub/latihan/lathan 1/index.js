1e738f5aec3bf6926fbad0f305a7349fea986c05
fajar <rokhman.fajar@gmail.com>
2025-10-24 04:13:54 +0700

    bersih2

--- basik/pub/latihan/lathan 1/index.js
+++
@@ -1,6 +0,0 @@
-//ini adalah komenter
-//komputer tidak akan menjalankannya
-//ini hanya untuk catatan saja
-tulis("Bismillah, Saya sedang belajar pemrograman");
-tulis("Nama saya Budi");
-tulis("Saya sekarang duduk di kelas 5");
