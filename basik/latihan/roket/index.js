9cf89422895a701b5dfcefa2c06daaeeed342ec3
fajar <rokhman.fajar@gmail.com>
2025-12-02 07:04:51 +0700

    folder latihan

---
+++ basik/latihan/roket/index.js
@@ -0,0 +1,24 @@
+let bg = muatImage("bg_bintang.jpg");
+let matahari = muatImage("matahari.png");
+let roket = muatImage("roket.png");
+
+matahari.x = 142;
+matahari.y = 142;
+matahari.panjang = 128;
+matahari.lebar = 128;
+matahari.pusatX = 64;
+matahari.pusatY = 64
+
+roket.pusatX = 32;
+roket.pusatY = 46
+
+function update() {
+  roket.x = mouseX();
+  roket.y = mouseY();
+  
+  matahari.rotasi++;
+  
+  gambarImage(bg);
+  gambarImage(matahari);
+  gambarImage(roket);
+}
