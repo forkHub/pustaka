4ebe55e03915b39dc06af105b1f25c1d91f961b8
fajar <rokhman.fajar@gmail.com>
2025-12-16 21:55:41 +0700

    hapus2

--- basik/basik/build.bat
+++
@@ -1,8 +0,0 @@
-echo ("build")
-call tsc -p .\tsconfig.json
-@REM echo  ("minify")
-@REM minify .\build\basik.js > .\build\basik.min.js
-node minify.js .\build\basik.js .\build\basik.min.js
-copy .\build\basik.min.js ..\build\basik.min.js /y
-pause
-pause
