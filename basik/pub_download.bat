ea499c37c464ed0567e4b66e7515060d0f9cdd80
fajar <rokhman.fajar@gmail.com>
2026-04-09 21:37:11 +0700

    a

--- basik/pub_download.bat
+++ basik/pub_download.bat
@@ -9,10 +9,18 @@ copy assets\*.* playground\web\asset || goto error
 copy build\*.* playground\web\editor\lib || goto error
 echo.
 
+del pub\*.* /s /q
+rd pub /s /q
+pause
+
 echo publikasi pg
 echo ============
-xcopy playground\web\*.* pub\ /s /i /y || goto error
+xcopy playground\web\*.* pub\basik /s /i /y || goto error
 echo.
+
+cd pub || goto error
+7z a -r -tzip basik basik\*.* || goto error
+cd..
 
 echo selesai
 goto end
