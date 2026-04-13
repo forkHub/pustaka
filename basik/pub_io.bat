f108a66604baa4a86e35e0dcff99d57ae88870e7
fajar <rokhman.fajar@gmail.com>
2026-04-13 23:20:31 +0700

    basik

--- basik/pub_io.bat
+++ basik/pub_io.bat
@@ -1,9 +1,11 @@
 @echo off
 echo off
 
+set io_folder=D:\xampp4\htdocs\io\basik
+
 echo publikasi stg
 echo ================
-xcopy stg\*.* ..\..\io\ /s /i /y || goto error
+xcopy stg\*.* %io_folder% /s /i /y || goto error
 echo.
 
 echo selesai
