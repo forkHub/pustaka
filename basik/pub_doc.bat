3aecd82e17930f862214393ef22440bbfacda3bc
fajar <rokhman.fajar@gmail.com>
2026-08-13 05:57:40 +0700

    web

--- basik/pub_doc.bat
+++ basik/pub_doc.bat
@@ -1,16 +1,16 @@
-@echo off
-echo off
+REM @echo off
+REM echo off
 
-echo publikasi doc
-echo =============
-xcopy doc\site\*.* stg\pg\doc /i /y /s || goto error
+REM echo publikasi doc
+REM echo =============
+REM xcopy doc\site\*.* stg\pg\doc /i /y /s || goto error
 
-echo selesai
-goto end
+REM echo selesai
+REM goto end
 
-:error
-pause
-pause
+REM :error
+REM pause
+REM pause
 
-:end
-pause
+REM :end
+REM pause
